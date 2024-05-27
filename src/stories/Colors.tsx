09260c2693e6b0beff18b0df71834779097e8346
Riley <riley@rileybathurst.com>
2024-05-27 15:57:31 -0700

    struggle puffing with the color card export

--- src/stories/Colors.tsx
+++ src/stories/Colors.tsx
@@ -1,6 +1,28 @@
 // this is the Name.tsx file
 import React from 'react';
-import { ColorCards } from 'color-cards';
+
+function ColorCards({ color, variables }) {
+  return (
+    <div>
+      <h2>{color}</h2>
+      {variables.map((variable) => (
+        <div
+          key={variable}
+          style={{
+            backgroundColor: `var(--${color}-${variable})`,
+            padding: '1rem',
+            margin: '1rem',
+            borderRadius: '0.5rem',
+            color: 'white',
+            textAlign: 'center',
+          }}
+        >
+          {`--${color}-${variable}`}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 interface ColorsProps {
   primary?: boolean;
@@ -18,7 +40,7 @@ export const Colors = ({
     >
       {primary ? 'primary' : 'secondary'}
       <ColorCards
-        color="red"
+        color="navy"
         variables={[100, 200]}
       />
     </main>
