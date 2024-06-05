d14235a48cc882a31f62bd015d0b468dbcb7d635
Riley <riley@rileybathurst.com>
2024-06-05 16:55:53 -0700

    spread import

--- src/PaddleTestimonials.tsx
+++ src/PaddleTestimonials.tsx
@@ -1,20 +1,18 @@
 import * as React from "react"
 
 interface TestimonialsTypes {
-  testimonials: TestimonialTypes[];
+  nodes: {
+    id: string;
+    testimonial: string;
+    sign: string;
+    customer: string;
+    location: string;
+  }[];
 }
 
-interface TestimonialTypes {
-  id: React.Key;
-  testimonial: string;
-  sign: string;
-  customer: string;
-  location: string;
-}
-
-export const PaddleTestimonials = ({ testimonials }: TestimonialsTypes) =>
+export const PaddleTestimonials = ({ nodes }: TestimonialsTypes) =>
   <ul className='testimonials vulture'>
-    {testimonials.map((testimonial: TestimonialTypes) => (
+    {nodes.map((testimonial) => (
       <li key={testimonial.id}>
         <p className='elbrus'><span className='denali font-serif'>&ldquo;</span>{testimonial.testimonial}<span className='denali font-serif'>&rdquo;</span></p>
         <p>{testimonial.sign} {testimonial.customer}</p>
