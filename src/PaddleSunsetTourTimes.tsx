e7970f9f23e80e47d6a8f94255bb942d30519b87
Riley <riley@rileybathurst.com>
2024-08-21 14:50:36 -0700

    feat: Add PaddleSunsetTourTimes component for displaying tour times

---
+++ src/PaddleSunsetTourTimes.tsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+
+type PaddleSunsetTourTimesTypes = {
+  nodes: {
+    id: string;
+    startDate: string;
+    endDate: string;
+    startTime: string;
+    endTime: string;
+  }[];
+}
+export const PaddleSunsetTourTimes = ({ nodes }: PaddleSunsetTourTimesTypes) =>
+  <section className="paddle-sunset-tour-times">
+    {/* // TODO: stylize start with a storybook */}
+    <p>*We adapt the time of our sunset tour to the sun</p>
+    {nodes.map((time) => {
+      const startDate = new Date(time.startDate);
+      const endDate = new Date(time.endDate);
+
+      const startTimeSplit = time.startTime.split(':');
+      const endTimeSplit = time.endTime.split(':');
+
+      return (
+        <div key={time.id}>
+          <p>{startDate.toLocaleDateString()} - {endDate.toLocaleDateString()}</p>
+          <p>{startTimeSplit[0]}:{startTimeSplit[1]} - {endTimeSplit[0]}:{endTimeSplit[1]}</p>
+        </div>
+      )
+    })}
+  </section>
---
+++ src/PaddleTopBar.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Markdown from 'react-markdown';
+
+// TODO this would be nice to be able to close but I dont have it right yet
+
+type PaddleTopBarTypes = {
+  strapiLocale: {
+    topbar: {
+      data: {
+        topbar: string;
+      }
+    }
+  };
+  allStrapiWeatherDay: {
+    nodes: {
+      startDate: string;
+      startTime: string;
+      endDate: string;
+      endTime: string;
+      closed: boolean;
+      condition: {
+        excerpt: string;
+      }
+    }[];
+  };
+}
+export const PaddleTopBar = ({ strapiLocale, allStrapiWeatherDay }: PaddleTopBarTypes) => {
+
+  let currentWeather = '';
+  let currentStatus = false;
+  const currentDate = new Date().toISOString().split('T')[0];
+
+  allStrapiWeatherDay.nodes.map((date) => {
+    if (date.startDate === currentDate || currentDate >= date.startDate && currentDate <= date.endDate) {
+      currentWeather = date.condition.excerpt;
+      currentStatus = date.closed;
+    }
+  });
+
+  return (
+    <div className={`top-bar ${currentStatus ? 'closed' : 'open'}`} >
+      {currentWeather ? <p>{currentWeather}</p> :
+        <Markdown>
+          {strapiLocale.topbar.data.topbar}
+        </Markdown>
+      }
+    </div>
+  )
+}
