600a8424ddb77faa71a0c92d7cd34c95d3fe4998
Riley <riley@rileybathurst.com>
2024-09-24 13:19:01 -0700

    phone

---
+++ src/phone.tsx
@@ -0,0 +1,32 @@
+import * as React from "react"
+
+interface FormatPhoneNumberTypes {
+  phoneNumberString: number;
+}
+function FormatPhoneNumber({ phoneNumberString }: FormatPhoneNumberTypes) {
+  const cleaned = `${phoneNumberString}`.replace(/\D/g, '');
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    return `(${match[1]}) ${match[2]}-${match[3]}`;
+  }
+  return null;
+}
+
+type PhoneTypes = {
+  phone: number;
+}
+
+const Phone = ({ phone }: PhoneTypes) => {
+
+  return (
+    <a
+      href={`tel:${phone}`}
+      rel="norel norefferer"
+      className="button"
+    >
+      Phone: <FormatPhoneNumber phoneNumberString={phone} />
+    </a>
+  )
+}
+
+export default Phone
