12ad06235e41b4b2e4a89a499c58e865465983db
Riley <riley@rileybathurst.com>
2024-09-24 11:05:37 -0700

    deck not card was the problem

--- src/PaddleLocationDeck.tsx
+++ src/PaddleLocationDeck.tsx
@@ -4,9 +4,11 @@ import type { LocationCardTypes } from "./types/location-card-types";
 
 interface LocationDeckTypes {
   background?: string;
+  season_start?: string;
+  season_end?: string;
   nodes: LocationCardTypes[];
 }
-export function PaddleLocationDeck({ nodes, background }: LocationDeckTypes) {
+export function PaddleLocationDeck({ nodes, season_start, season_end, background }: LocationDeckTypes) {
   return (
     <section className="location-deck">
       {nodes.map((location: LocationCardTypes) => (
@@ -14,6 +16,8 @@ export function PaddleLocationDeck({ nodes, background }: LocationDeckTypes) {
           key={location.id}
           {...location}
           background={background}
+          season_start={season_start}
+          season_end={season_end}
         />
       ))}
     </section>
