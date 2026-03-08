20e30491a5b8439bdd06c4349d0c1538feee5657
Riley Bathurst <riley@rileybathurst.com>
2026-03-08 14:37:11 -0700

    brand list when I filter before

--- src/paddle-brand-list.tsx
+++ src/paddle-brand-list.tsx
@@ -3,34 +3,11 @@ import { Link } from 'gatsby';
 import SVG from 'react-inlinesvg';
 import type { PaddleBrandType } from "./types/paddle-brand-types";
 
-// * additional work is done here to split kayak and paddleboard
-
 export const PaddleBrandList = ({ nodes, sport }: { nodes: PaddleBrandType[]; sport?: string }) => {
-
-  const BrandSet = new Set<PaddleBrandType>();
-  let BrandArray: PaddleBrandType[] = [];
-
-  if (sport) {
-    nodes.map((brand) => {
-      brand.retail.map((retail) => {
-
-        if (retail.sport.slug === sport) {
-          BrandSet.add(brand);
-        }
-
-        return BrandSet;
-      })
-    });
-
-    BrandArray = (Array.from(BrandSet));
-  } else {
-    // TODO: this needs a check if anything is published in the brand if no sport is passed
-    BrandArray = (Array.from(nodes));
-  }
 
   return (
     <ul className='brand_list'>
-      {BrandArray.map((brand: PaddleBrandType) => (
+      {nodes.map((brand: PaddleBrandType) => (
         <li key={brand.id}>
           <Link to={`/retail/${sport}/${brand.slug}`}>
             {brand.svg ?
