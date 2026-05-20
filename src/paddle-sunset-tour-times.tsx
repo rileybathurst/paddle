db111554c15f6e64365a663c843ef80920f5a620
Riley Bathurst <riley@rileybathurst.com>
2026-05-20 16:40:33 -0700

    AM PM sunset times

--- src/paddle-sunset-tour-times.tsx
+++ src/paddle-sunset-tour-times.tsx
@@ -1,5 +1,22 @@
 import * as React from "react"
 
+const formatTimeTo12Hour = (timeValue: string) => {
+  const [hoursRaw, minutesRaw = "00"] = timeValue.split(':');
+  const hoursNum = Number(hoursRaw);
+  const minutesNum = Number(minutesRaw);
+
+  if (Number.isNaN(hoursNum) || Number.isNaN(minutesNum)) {
+    return timeValue;
+  }
+
+  const normalizedHours = ((hoursNum % 24) + 24) % 24;
+  const period = normalizedHours >= 12 ? "PM" : "AM";
+  const displayHours = normalizedHours % 12 || 12;
+  const displayMinutes = String(minutesNum).padStart(2, "0");
+
+  return `${displayHours}:${displayMinutes} ${period}`;
+}
+
 type PaddleSunsetTourTimesTypes = {
   nodes: {
     id: React.Key;
@@ -16,13 +33,17 @@ export const PaddleSunsetTourTimes = ({ nodes }: PaddleSunsetTourTimesTypes) =>
       const startDate = new Date(time.startDate);
       const endDate = new Date(time.endDate);
 
-      const startTimeSplit = time.startTime.split(':');
-      const endTimeSplit = time.endTime.split(':');
-
-      // Jan 26 - May 31
-      // 20:1 - 5:17
+      const formattedStartTime = formatTimeTo12Hour(time.startTime);
+      const formattedEndTime = formatTimeTo12Hour(time.endTime);
 
       const isPassed = endDate < new Date();
+      const timeRange = (
+        <>
+          {formattedStartTime}
+          &nbsp;-&nbsp;
+          {formattedEndTime}
+        </>
+      );
 
       return (
         <div key={time.id} className="date-time">
@@ -40,19 +61,7 @@ export const PaddleSunsetTourTimes = ({ nodes }: PaddleSunsetTourTimesTypes) =>
             {endDate.toLocaleString("en-US", { day: "numeric" })}
           </h3>
           <p className="time">
-            {isPassed ? (
-              <del>
-                {startTimeSplit[0]}:{startTimeSplit[1]}
-                &nbsp;-&nbsp;
-                {endTimeSplit[0]}:{endTimeSplit[1]}
-              </del>
-            ) : (
-              <>
-                {startTimeSplit[0]}:{startTimeSplit[1]}
-                &nbsp;-&nbsp;
-                {endTimeSplit[0]}:{endTimeSplit[1]}
-              </>
-            )}
+            {isPassed ? <del>{timeRange}</del> : timeRange}
           </p>
         </div>
       )
